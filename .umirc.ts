f07d9778fd0724a6f016c600877159d3b65bb4f8
wqj <3167284533@qq.com>
2019-12-24 22:13:36 +0800

    umi

---
+++ .umirc.ts
@@ -0,0 +1,38 @@
+import { IConfig } from 'umi-types'; // ref: https://umijs.org/config/
+
+const config: IConfig = {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        {
+          path: '/',
+          component: './index',
+        },
+        {
+          path: '/',
+          component: '../pages/index',
+        },
+      ],
+    },
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: false,
+        title: 'wqj-umi',
+        dll: false,
+        routes: {
+          exclude: [/components\//],
+        },
+      },
+    ],
+  ],
+};
+export default config;
