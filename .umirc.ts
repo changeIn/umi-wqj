bfe5dc09ee74f474769b25b0f9ae19b90dfc50bc
wqj <3167284533@qq.com>
2019-12-25 17:26:33 +0800

    umi-plugin-react 插件集配置

--- .umirc.ts
+++ .umirc.ts
@@ -31,19 +31,45 @@ const config: IConfig = {
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react',{
         antd: true,
-        dva: true,
-        dynamicImport: false,
+        dva: {
+          immer: true,
+        },
+        // dynamicImport: false,
+        dynamicImport: {
+          webpackChunkName: true,
+          loadingComponent: './components/Loading.js',
+        },
         locale: {
           enable: true,
         },
         title: 'wqj-umi',
-        dll: false,
+        // dll: false,
+        dll: {
+          exclude: [],
+        },
         routes: {
           exclude: [/components\//],
         },
-    }],
-  ],
 
+        polyfills: ['ie9'],
+        library: 'react',
+        pwa: true,
+        hd: true,
+        fastClick: true,
+        chunks: ['vendor', 'umi'],
+        scripts: [
+          { src: 'http://cdn/a.js' },
+          { src: '<%= PUBLIC_PATH %>a.js' },
+          { content: `alert('a');` },
+        ],
+        headScripts: [],
+        metas: [{ charset: 'utf-8' }],
+        links: [{ rel: 'stylesheet', href: 'http://cdn/a.css' }],
+
+
+    }],
+
+  ],
 
 };
 export default config;
